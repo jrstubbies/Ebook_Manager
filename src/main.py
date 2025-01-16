d85084c6743d172bf6daf8503bcfa167708bf6a0
Myles Stubbs <jrstubbies@gmail.com>
2025-01-16 18:35:27 +1300

    added basic menu code to main.py

--- src/main.py
+++ src/main.py
@@ -2,25 +2,104 @@ import flet as ft
 
 
 def main(page: ft.Page):
-    counter = ft.Text("0", size=50, data=0)
+    appbar_text_ref = ft.Ref[ft.Text]()
 
-    def increment_click(e):
-        counter.data += 1
-        counter.value = str(counter.data)
-        counter.update()
+    # on click of menu item will display what button was clicked down the bottom of the window
+    def handle_menu_item_click(e):
+        print(f"{e.control.content.value}.on_click")
+        page.open(ft.SnackBar(content=ft.Text(f"{e.control.content.value} was clicked!")))
+        appbar_text_ref.current.value = e.control.content.value
+        page.update()
 
-    page.floating_action_button = ft.FloatingActionButton(
-        icon=ft.Icons.ADD, on_click=increment_click
-    )
-    page.add(
-        ft.SafeArea(
-            ft.Container(
-                counter,
-                alignment=ft.alignment.center,
+    # in the command terminal will print when a submenu is opened
+    def handle_submenu_open(e):
+        print(f"{e.control.content.value}.on_open")
+
+    # in the command terminal will print when a submenu is closed
+    def handle_submenu_close(e):
+        print(f"{e.control.content.value}.on_close")
+        
+
+    menubar = ft.MenuBar(
+        expand=True,
+        style=ft.MenuStyle(
+            alignment=ft.alignment.top_left,
+            bgcolor=ft.Colors.RED_300,
+            mouse_cursor={
+                ft.ControlState.HOVERED: ft.MouseCursor.WAIT,
+                ft.ControlState.DEFAULT: ft.MouseCursor.ZOOM_OUT,
+            },
+        ),
+        controls=[
+            ft.SubmenuButton(
+                content=ft.Text("File"),
+                on_open=handle_submenu_open,
+                on_close=handle_submenu_close,
+                controls=[
+                    ft.MenuItemButton(
+                        content=ft.Text("About"),
+                        leading=ft.Icon(ft.Icons.INFO),
+                        style=ft.ButtonStyle(
+                            bgcolor={ft.ControlState.HOVERED: ft.Colors.GREEN_100}
+                        ),
+                        on_click=handle_menu_item_click,
+                    ),
+                    ft.MenuItemButton(
+                        content=ft.Text("Save"),
+                        leading=ft.Icon(ft.Icons.SAVE),
+                        style=ft.ButtonStyle(
+                            bgcolor={ft.ControlState.HOVERED: ft.Colors.GREEN_100}
+                        ),
+                        on_click=handle_menu_item_click,
+                    ),
+                    ft.MenuItemButton(
+                        content=ft.Text("Quit"),
+                        leading=ft.Icon(ft.Icons.CLOSE),
+                        style=ft.ButtonStyle(
+                            bgcolor={ft.ControlState.HOVERED: ft.Colors.GREEN_100}
+                        ),
+                        on_click=handle_menu_item_click,
+                    ),
+                ],
             ),
-            expand=True,
-        )
+            ft.SubmenuButton(
+                content=ft.Text("View"),
+                on_open=handle_submenu_open,
+                on_close=handle_submenu_close,
+                controls=[
+                    ft.SubmenuButton(
+                        content=ft.Text("Zoom"),
+                        controls=[
+                            ft.MenuItemButton(
+                                content=ft.Text("Magnify"),
+                                leading=ft.Icon(ft.Icons.ZOOM_IN),
+                                close_on_click=False,
+                                style=ft.ButtonStyle(
+                                    bgcolor={
+                                        ft.ControlState.HOVERED: ft.Colors.PURPLE_200
+                                    }
+                                ),
+                                on_click=handle_menu_item_click,
+                            ),
+                            ft.MenuItemButton(
+                                content=ft.Text("Minify"),
+                                leading=ft.Icon(ft.Icons.ZOOM_OUT),
+                                close_on_click=False,
+                                style=ft.ButtonStyle(
+                                    bgcolor={
+                                        ft.ControlState.HOVERED: ft.Colors.PURPLE_200
+                                    }
+                                ),
+                                on_click=handle_menu_item_click,
+                            ),
+                        ],
+                    )
+                ],
+            ),
+        ],
     )
+
+    page.add(ft.Row([menubar]))
 
 
 ft.app(main)
